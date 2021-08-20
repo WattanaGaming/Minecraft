e34cae7e627e7e973522ef8cbcef3f2da8b2ffb1
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2021-08-20 11:29:22 +0700

    Update MTWorks

--- mods/mineport/init.lua
+++ mods/mineport/init.lua
@@ -2,8 +2,26 @@ mineport = {
     path = mtworks.get_current_modpath(),
 }
 
+---@param overrides table
+---@return table
+function mineport.defaultGroupcaps(overrides)
+    local groupcaps = {
+        oddly_breakable_by_hand = {times={[1]=3.50,[2]=2.00,[3]=0.70}, uses=0}, -- Kept for testing
+        grass = {times = {0.9}},
+        crumbly = {times = {0.75}},
+        wood = {times = {3}},
+        leaf = {times = {0.3}},
+        plant = {times = {0}},
+    }
+    if overrides then
+        return mtworks.override_table(groupcaps, overrides)
+    else
+        return groupcaps
+    end
+end
+
 minetest.log("action", "Running scripts")
-mtworks.dirTree(mineport.path.."/scripts", function(path)
+mtworks.dir_tree(mineport.path.."/scripts", function(path)
     if path:match("^.+(%..+)$") == ".lua" then
         minetest.log("action", "> "..path)
         dofile(path)
--- mods/mtworks/init.lua
+++ mods/mtworks/init.lua
@@ -8,10 +8,20 @@ function mtworks.get_current_modpath()
     return minetest.get_modpath(minetest.get_current_modname())
 end
 
+---@param original table
+---@param overrides table
+function mtworks.override_table(original, overrides)
+    assert(type(original) == "table" and type(overrides) == "table")
+    for key,value in pairs(overrides) do
+        original[key] = value
+    end
+    return original
+end
+
 -- Recursively iterate through files in a specified directory.
 ---@param dir string
 ---@param fileHandler fun(path: string)
-function mtworks.dirTree(dir, fileHandler)
+function mtworks.dir_tree(dir, fileHandler)
     assert(dir, "directory must be specified")
     fileHandler = fileHandler or function() end
 
@@ -24,6 +34,6 @@ function mtworks.dirTree(dir, fileHandler)
     -- Directories.
     local directories = minetest.get_dir_list(dir, true)
     for _,directory in ipairs(directories) do
-        mtworks.dirTree(dir.."/"..directory, fileHandler)
+        mtworks.dir_tree(dir.."/"..directory, fileHandler)
     end
 end
