5eb18ca4e0b5363ee110e7501b238c04744b7ff6
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2021-08-22 13:22:36 +0700

    Put biomes with similiar climates into one file

--- mods/mineport/scripts/biomes/temperate_biomes.lua
+++ mods/mineport/scripts/biomes/temperate_biomes.lua
@@ -13,3 +13,18 @@ minetest.register_biome({
     heat_point = 25,
     humidity_point = 0.8,
 })
+
+minetest.register_biome({
+    name = "plains",
+
+    node_top = "mineport:grass_block",
+    depth_top = 1,
+
+    node_filler = "mineport:dirt",
+    depth_filler = 3,
+
+    y_min = 1,
+
+    heat_point = 25,
+    humidity_point = 0.7,
+})
