46d3454614da37d68a029dc2a9cbadb42ebdd916
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2021-08-24 19:44:35 +0700

    Move settings n stuff

--- mods/core/init.lua
+++ mods/core/init.lua
@@ -1,29 +1,22 @@
 mineport = {
     path = utils.get_current_modpath(),
-    water_range = 7,
-    water_viscosity = 0,
-    water_drowning = 1,
-}
-mineport.colors = {
-    water_post_effect = "#57c5d840"
-}
 
----@param overrides table
----@return table
-function mineport.defaultGroupcaps(overrides)
-    local groupcaps = {
+    colors = {
+        water_post_effect = "#57c5d840"
+    },
+
+    default_group_caps = {
         oddly_breakable_by_hand = {times={[1]=3.50,[2]=2.00,[3]=0.70}, uses=0}, -- Kept for testing
         grass = {times = {0.9,0.45,0.25,0.15,0.15,0.1,0.1}},
         dirt = {times = {0.75,0.4,0.2,0.15,0.1,0.1,0.1}},
         stone = {times = {7.5,1.15,0.6,0.4,0.3,0.25,0.2}},
         sand = {times = {0.75,0.4,0.2,0.15,0.1,0.1,0.1}},
-    }
-    if overrides then
-        return utils.override_table(groupcaps, overrides)
-    else
-        return groupcaps
-    end
-end
+    },
+
+    water_range = 7,
+    water_viscosity = 0,
+    water_drowning = 1,
+}
 
 minetest.log("action", "Running scripts")
 utils.dir_tree(mineport.path.."/scripts", function(path)
--- mods/core/scripts/default_tool.lua
+++ mods/core/scripts/default_tool.lua
@@ -3,7 +3,7 @@ minetest.register_tool(":", {
     wield_image = "wieldhand.png",
     tool_capabilities = {
         max_drop_level = 0,
-        groupcaps = mineport.defaultGroupcaps(),
+        groupcaps = mineport.default_group_caps,
         damage_groups = {fleshy=1},
     }
 })
